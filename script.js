88557169926c9c2892eb1b07118f2dabcc0e74cf
rachelsmpark <156369192+rachelsmpark@users.noreply.github.com>
2025-11-06 17:00:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFjaGVsc21wYXJrIiwiYSI6ImNtaDlyZjNreTFjYnoybXB1bHJqa29lZWgifQ.zynQjHCJTpEk_7Po2XEhZA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/rachelsmpark/cmho4lbl9000801suf7us1rcg',
+  center: [-122.2748, 37.8685],
+  zoom: 10
+});
